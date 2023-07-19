989750d2b7cff5848f4f6ad8fe88740641747e4e
Thiago Pessoa Medeiros <thiagopessoa@sefaz.pi.gov.br>
2023-07-19 11:56:19 -0300

    conectando api

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -2,13 +2,27 @@ import Input from '../form/Input'
 import Select from '../form/Select'
 import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
+import {UseState, useState} from 'react'
 
 function ProjectForm({btnText}){
+
+    const [categories, setCategories] = useState([])
+
+    fetch("http://localhost:5000/categories", {
+        method: "GET",
+        headers: {
+            "Content-type": "application/json"
+        }
+    }).then((resp) => resp.json())
+    .then((data) =>{
+        setCategories(data)
+    })
+    .catch(err => console.log(err))
     return (
         <form className={styles.form}>
             <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto"/>
             <Input type="number" text="Budget" name="name" placeholder="Insira o orçamento total"/>
-            <Select name="category_id" text="Selecione a categoria"/>
+            <Select name="category_id" text="Selecione a categoria" options="1"/>
             <SubmitButton text={"Criar Projeto"}/>
         </form>
     )
