bb5e2dd02f624256eef10cecacaf69d90e4f8e23
Thiago Pessoa Medeiros <thiagopessoa@sefaz.pi.gov.br>
2023-07-19 11:27:17 -0300

    Criando Formulário

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,19 +1,15 @@
 import Input from '../form/Input'
+import Select from '../form/Select'
+import SubmitButton from '../form/SubmitButton'
 import styles from './ProjectForm.module.css'
 
-function ProjectForm(){
+function ProjectForm({btnText}){
     return (
         <form className={styles.form}>
             <Input type="text" text="Nome do projeto" name="name" placeholder="Insira o nome do projeto"/>
             <Input type="number" text="Budget" name="name" placeholder="Insira o orçamento total"/>
-            <div>
-            <select name="category_id">
-                <option disabled>Selecione a categoria</option>
-            </select>
-            </div>
-            <div>
-                <input type="submit"/>
-            </div>
+            <Select name="category_id" text="Selecione a categoria"/>
+            <SubmitButton text={"Criar Projeto"}/>
         </form>
     )
 }
