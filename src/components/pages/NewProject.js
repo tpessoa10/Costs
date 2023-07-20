1358beb0eba6b52dd229c4d4936a44bf31acc93a
Thiago <tpessoa96@gmail.com>
2023-07-19 21:04:31 -0300

    inserindo dados

--- src/components/pages/NewProject.js
+++ src/components/pages/NewProject.js
@@ -1,7 +1,17 @@
 import ProjectForm from '../project/ProjectForm'
 import styles from './NewProject.module.css'
+import {useNavigate} from 'react-router-dom'
 
 function NewProject(){
+
+    const history = useNavigate()
+
+    function createPost(project){
+        //initialize cost and services
+        project.cost = 0
+        project.services = []
+    }
+
     return (
         <div className={styles.newproject_container}>
             <h1>Criar Projeto</h1>
