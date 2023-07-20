9ee5631aaff2513338803919b855fdaf34ea2251
Thiago Pessoa Medeiros <thiagopessoa@sefaz.pi.gov.br>
2023-07-20 11:58:26 -0300

    Resgatando do banco

---
+++ src/components/project/ProjectCard.js
@@ -0,0 +1,10 @@
+import styles from './ProjectCard.module.css'
+import {BsPencil, BsFillTrashFill} from 'react-icons/bs'
+
+function ProjectCard ({id, name, budget, category, handleRemove}) {
+    return (
+        <p>Projeto</p>
+    )
+}
+
+export default ProjectCard
