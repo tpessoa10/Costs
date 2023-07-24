816b237a320e90fde84f1141a4f51fda0abffb5a
Thiago Pessoa Medeiros <thiagopessoa@sefaz.pi.gov.br>
2023-07-24 11:28:44 -0300

    Pagina de edicao de dados

--- src/components/pages/Projects.js
+++ src/components/pages/Projects.js
@@ -1,6 +1,6 @@
 import Message from "../layout/Message"
 import {useLocation} from 'react-router-dom'
-import styles from './Project.module.css'
+import styles from './Projects.module.css'
 import Container from "../layout/Container"
 import LinkButton from '../layout/LinkButton'
 import ProjectCard from '../project/ProjectCard'
